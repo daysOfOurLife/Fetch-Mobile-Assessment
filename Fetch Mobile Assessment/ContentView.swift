1a1088ef0558982d9c5e2b1f85be4eecfb3c5c70
Victor Nguyen <91512538+daysOfOurLife@users.noreply.github.com>
2023-09-28 14:01:54 -0500

    Grouping items by listId

--- Fetch Mobile Assessment/ContentView.swift
+++ Fetch Mobile Assessment/ContentView.swift
@@ -25,11 +25,17 @@ struct ContentView: View {
 
     var body: some View {
         List {
-            ForEach(items, id: \.id) { item in
-                if let itemName = item.name {
-                    if !itemName.isEmpty {
-                        Text("listId:\(item.listId), id:\(item.id), name:\(itemName)")
+            ForEach(1 ... 4, id: \.self) { listId in
+                Section {
+                    ForEach(items.filter { $0.listId == listId }, id: \.id) { item in
+                        if let itemName = item.name {
+                            if !itemName.isEmpty {
+                                Text("listId:\(item.listId), id:\(item.id), name:\(itemName)")
+                            }
+                        }
                     }
+                } header: {
+                    Text("listId: \(listId)")
                 }
             }
         }
@@ -57,7 +63,7 @@ struct ContentView: View {
 
         let (data, _) = try await URLSession.shared.data(from: url)
 
-        // Parse JSON data into a collection of ``Item``s and assign to `items`
+        // Parse JSON data into a collection of ``Item``s and assign to `items`.
         do {
             items = try JSONDecoder().decode([Item].self, from: data)
         } catch {
@@ -65,7 +71,7 @@ struct ContentView: View {
         }
     }
 
-    /// Sorts by item listId. If listIds are equal, sort by item name
+    /// Sorts by item listId. If listIds are equal, sort by item name.
     func customSort() {
         items.sort { item1, item2 in
             if item1.listId == item2.listId {
@@ -74,6 +80,15 @@ struct ContentView: View {
                 return item1.listId < item2.listId
             }
         }
+    }
+
+    /// Filters collection of ``Item``s that have the specified listId
+    ///
+    /// - Parameter listsId: listId property of ``Item``
+    ///
+    /// - Returns: A collection of ``Item``s that have the specified listId
+    func filteredItems(by listId: Int) -> [Item] {
+        return items.filter { $0.listId == listId }
     }
 }
 
